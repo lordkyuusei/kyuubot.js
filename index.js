0660667661ee4e7384c4c9d9ca34e073f0ca4919
Kevin POIROT <kevin.poirot@advanzia.com>
2020-10-19 15:53:27 +0200

    chore: updateRole method + boolean for add/remove to avoid code duplication

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const config = require('./config.json');
 
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-client.once('ready', () => {
-    console.log("Ready!");
-})
+client.once('ready', () => {})
 
 const filter = (message_id) => message_id === config.roles.message_id;
 
@@ -16,7 +14,7 @@ client.on('guildMemberAdd', member => {
     console.log(`Successfully added role ${role.name} to ${member.nickname || member.user.username}!`);
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
+const prepare = async (reaction) => {
     const { id } = reaction.message;
     if (!filter(id)) return;
 
@@ -27,51 +25,56 @@ client.on('messageReactionAdd', async (reaction, user) => {
 			console.error('Something went wrong when fetching the message: ', error);
 			return;
 		}
-	}
+    }
+}
 
+const updateRole = (reaction, user, isAdd) => {
     const pair = config.reactionsToRoleMap.find(pair => pair.emoji === reaction._emoji.name);
+
     if (pair) {
         const { channel } = reaction.message;
         const role = channel.guild.roles.cache.find(role => role.name === pair.role);
         const member = channel.guild.members.cache.find(member => member.id === user.id);
+
+        // If the role is not assigned...
         if (!member.roles.cache.has(role.id)) {
-            member.roles.add(role);
-            console.log(`Successfully added role ${role.name} to ${member.nickname || member.user.username}!`);
-        } else {
-            console.log(`${role.name} is already assigned to ${member.nickname || member.user.username}!`);
+            // ... and we want it to be, then we add it
+            if (isAdd) {
+                member.roles.add(role);
+                console.log(`Successfully added role ${role.name} to ${member.nickname || member.user.username}!`);
+            }
+            // Otherwise, we don't do anything.
+            else {
+                console.log(`${role.name} is not assigned to ${member.nickname || member.user.username}!`);
+            }
+        } 
+        
+        // If the role is assigned...
+        else {
+            // ... but we don't want it to be, then we remove it
+            if (!isAdd) {
+                member.roles.remove(role);
+                console.log(`Successfully removed role ${role.name} to ${member.nickname || member.user.username}!`);    
+            }
+            
+            // Otherwise, guess what
+            else {
+                console.log(`${role.name} is already assigned to ${member.nickname || member.user.username}!`);
+            }
         }
     } else {
         console.error(`${reaction._emoji.name} doesn't enable any role.`);
     }
+}
+
+client.on('messageReactionAdd', (reaction, user) => {
+    prepare(reaction);
+    updateRole(reaction, user, true);
 });
 
-client.on('messageReactionRemove', async (reaction, user) => {
-    const { id } = reaction.message;
-    if (!filter(id)) return;
-
-	if (reaction.partial) {
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.error('Something went wrong when fetching the message: ', error);
-			return;
-		}
-	}
-
-    const pair = config.reactionsToRoleMap.find(pair => pair.emoji === reaction._emoji.name);
-    if (pair) {
-        const { channel } = reaction.message;
-        const role = channel.guild.roles.cache.find(role => role.name === pair.role);
-        const member = channel.guild.members.cache.find(member => member.id === user.id);
-        if (member.roles.cache.has(role.id)) {
-            member.roles.remove(role);
-            console.log(`Successfully removed role ${role.name} to ${member.nickname || member.user.username}!`);
-        } else {
-            console.log(`${role.name} is not assigned to ${member.nickname || member.user.username}!`);
-        }
-    } else {
-        console.error(`${reaction._emoji.name} doesn't enable any role.`);
-    }
+client.on('messageReactionRemove', (reaction, user) => {
+    prepare(reaction);
+    updateRole(reaction, user, false);
 });
 
 client.login(config.meta.token);

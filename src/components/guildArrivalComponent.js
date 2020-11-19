6850b3b7d66325b7b6c750c85ff524b04617a0a2
Kyuusei <kevin.poirot@epitech.eu>
2020-11-19 10:08:55 +0100

    Update guildArrivalComponent.js
    fix(config roles): use wrong property (name instead of role)

--- src/components/guildArrivalComponent.js
+++ src/components/guildArrivalComponent.js
@@ -1,6 +1,6 @@
 const guildMemberAdd = (config, member) => {
     const guest = config.reactionsToRoleMap.find(role => role.emoji === '🍁');
-    const role = member.guild.roles.cache.find(role => role.name === guest.name);
+    const role = member.guild.roles.cache.find(role => role.name === guest.role);
     member.roles.add(role);
     console.log(`Successfully added role ${role.name} to ${member.nickname || member.user.username}!`);
 }

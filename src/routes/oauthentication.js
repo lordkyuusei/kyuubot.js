4c5d64ea73cfc77fe9b694df7222b0e81b79cd87
Kevin POIROT <kpoirot@positivethinking.lu>
2021-06-05 12:37:30 +0200

    feat(access_token): get access token

--- src/routes/oauthentication.js
+++ src/routes/oauthentication.js
@@ -2,12 +2,18 @@ const oauthenticationComponent = (twitch) => {
     const oauthenticationRoute = "/api/twitch/oauth";
     const twitchOauth = `https://id.twitch.tv/oauth2/authorize?client_id=${twitch.twitchId}&redirect_uri=${twitch.twitchRu}&response_type=code&scope=${twitch.twitchSc}`;
     const oauthenticationCallback = (req, res) => {
-        if (req.query !== "") {
-            const { code, scope } = req.query;
-            res.send(`code: ${code}`);
-        } else {
+        const { code, accessToken, scope } = req.query;
+        if (code === null && accessToken === null)
             res.send(`<a href="${twitchOauth}">Connect to Twitch</a>`)            
+        else if (accessToken === null) {
+            const url = `https://id.twitch.tv/oauth2/token?client_id=${twitch.twitchId}&client_secret=${twitch.St}&code=${code}&grant_type=authorization_code&redirect_uri=${twitch.twitchRu}`;
+            fetch(url, { method: "POST"}).then(response => {
+                console.log(response);
+                res.send(`token: ${response.access_token} (use ${response.refresh_token} to get new). expires in ${response.expires_in}s`);
+            })
         }
+        else
+            res.send(`access token: ${accessToken}`);
     }
     return [oauthenticationRoute, oauthenticationCallback];
 }

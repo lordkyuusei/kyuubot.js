7d1ede7fdaaeac91475d81df001e4ef19d52dfd3
Kevin POIROT <kpoirot@positivethinking.lu>
2021-06-05 19:07:54 +0200

    fix(hmacSign): from pure js function to node-js crypto lib

--- src/store/hmac.js
+++ src/store/hmac.js
@@ -1,13 +1,5 @@
-import crypto from "crypto";
+import Crypto from "crypto";
 
-const hmacSign = async (key, message) => {
-    const g = (str) => new Uint8Array([...unescape(encodeURIComponent(str))].map(c => c.charCodeAt(0)));
-    const k = g(key);
-    const m = g(message);
-    const c = await crypto.subtle.importKey('raw', k, { name: "HMAC", hash: "SHA-256" }, true, ['sign']);
-    const s = await crypto.subtle.sign("HMAC", c, m);
-    [...new Uint8Array(s)].map(b => b.toString(16).padStart(2, '0')).join('');
-    return btoa(String.fromCharCode(...new Uint8Array(s)));
-}
+const hmacSign = (key, message) => Crypto.createHmac('sha256', key).update(message).digest('hex');
 
 export default hmacSign;

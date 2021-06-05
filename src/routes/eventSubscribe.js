9987f00ae00016b010526c3e74c38936764414ec
Kevin POIROT <kpoirot@positivethinking.lu>
2021-06-05 18:51:04 +0200

    feat(event callback): try to compare challenges

--- src/routes/eventSubscribe.js
+++ src/routes/eventSubscribe.js
@@ -1,4 +1,5 @@
 import { subscribeEvent } from "../store/requests";
+import hmacSign from "../store/hmac";
 
 export const authorizationComponent = ({ id, eventCallback, secret}) => {
     const authorizationRoute = "/api/twitch/eventSubscribe";
@@ -13,12 +14,23 @@ export const authorizationComponent = ({ id, eventCallback, secret}) => {
     return [authorizationRoute, authorizationCallback];
 }
 
-export const validationComponent = () => {
+export const validationComponent = (secret) => {
     const validationRoute = "/api/twitch/event";
     const validationCallback = (req, res) => {
-        console.log(req.rawHeaders);
+        console.log(req);
+        const id = req.rawHeaders["Twitch-Eventsub-Message-Id"];
+        const ts = req.rawHeaders["Twitch-Eventsub-Message-Timestamp"];
+        const sn = req.rawHeaders["Twitch-Eventsub-Message-Signature"];
+
+        const hmac = await hmacSign(secret, `${id}${ts}${req.body || ''}`);
+        const challenge = `sha256=${hmac.toString(16)}`;
+
+        if (sn !== challenge) {
+            console.log("EUH LA !!!");
+        } else {
+            console.log("omg c passé");
+        }
         res.send("<h1>ok</h1>");
     }
     return [validationRoute, validationCallback];
 }
-export default authorizationComponent;
---
+++ src/store/hmac.js
@@ -0,0 +1,11 @@
+const hmacSign = async (key, message) => {
+    const g = (str) => new Uint8Array([...unescape(encodeURIComponent(str))].map(c => c.charCodeAt(0)));
+    const k = g(key);
+    const m = g(message);
+    const c = await crypto.subtle.importKey('raw', k, { name: "HMAC", hash: "SHA-256" }, true, ['sign']);
+    const s = await crypto.subtle.sign("HMAC", c, m);
+    [...new Uint8Array(s)].map(b => b.toString(16).padStart(2, '0')).join('');
+    return btoa(String.fromCharCode(...new Uint8Array(s)));
+}
+
+export default hmacSign;

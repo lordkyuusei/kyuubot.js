986ace1766c5df4d35b4e1e5dcbebec9fbae524e
Kevin POIROT <kpoirot@positivethinking.lu>
2021-06-05 12:27:24 +0200

    feat(code): getting code from twitch

--- src/routes/oauthentication.js
+++ src/routes/oauthentication.js
@@ -1,9 +1,13 @@
 const oauthenticationComponent = (twitch) => {
     const oauthenticationRoute = "/api/twitch/oauth";
     const twitchOauth = `https://id.twitch.tv/oauth2/authorize?client_id=${twitch.twitchId}&redirect_uri=${twitch.twitchRu}&response_type=code&scope=${twitch.twitchSc}`;
-    const oauthenticationCallback = (req, res) =>{
-        console.log(req);
-        res.send(`<a href="${twitchOauth}">Connect to Twitch</a>`)
+    const oauthenticationCallback = (req, res) => {
+        if (req.query !== "") {
+            const { code, scope } = req.query;
+            res.send(`code: ${code}`);
+        } else {
+            res.send(`<a href="${twitchOauth}">Connect to Twitch</a>`)            
+        }
     }
     return [oauthenticationRoute, oauthenticationCallback];
 }

7badcee378eecb9cb3e2e5d327d6ddfb1ce95fbe
Kevin POIROT <kpoirot@positivethinking.lu>
2021-06-27 19:42:51 +0200

    fix(updates): fix when there is no message

--- src/components/updatesManagement.js
+++ src/components/updatesManagement.js
@@ -11,7 +11,7 @@ const handleUpdates = ({ channels }, config, version) => {
     const channel = config.updates.channel_id;
     const updatesChannel = channels.cache.get(channel);
     const messages = updatesChannel.messages.fetch({ limit: 1 }).then(messages => {
-        const lastChangelog = messages.first();
+        const lastChangelog = messages.first() ?? "";
         const isChangelogLastVersion = lastChangelog.content.includes(version);
         return isChangelogLastVersion ? 0 : publishLastUpdates(updatesChannel, version);
     })
